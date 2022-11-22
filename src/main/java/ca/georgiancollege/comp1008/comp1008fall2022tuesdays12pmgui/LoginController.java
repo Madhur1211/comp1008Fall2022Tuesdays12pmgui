5153dfd69eff40990b77712eececa78d937f2e3b
Ben Blanc <ben.blanc@georgebrown.ca>
2022-11-22 13:58:15 -0500

    Nov 22, 2022 code @ 14h

--- src/main/java/ca/georgiancollege/comp1008/comp1008fall2022tuesdays12pmgui/LoginController.java
+++ src/main/java/ca/georgiancollege/comp1008/comp1008fall2022tuesdays12pmgui/LoginController.java
@@ -2,6 +2,7 @@ package ca.georgiancollege.comp1008.comp1008fall2022tuesdays12pmgui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -22,6 +23,35 @@ public class LoginController {
     void onLoginClick(ActionEvent event) {
 
         System.out.println("You click on Login button");
+
+        String receivedUsername = textUsername.getText();
+        String receivedPassword = textPassword.getText();
+
+        String expectedUsername = "admin", expectedPassword = "pass";
+
+        if(receivedUsername.equals(expectedUsername)
+        &&
+        receivedPassword.equals(expectedPassword)
+        ){
+
+            Utilities.showAlert(Alert.AlertType.INFORMATION, "Good job!", "You may enter").show();
+
+            try{
+                Utilities.openPage("welcome", "Welcome Everyone!");
+            }
+            catch (Exception e){
+                Utilities.showAlert(Alert.AlertType.ERROR, "Not Found", "Could not load the page").show();
+            }
+
+        }
+        else{
+            System.out.println("bad!");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("No good!");
+            alert.setContentText("Invalid username and/or password");
+            alert.show();
+        }
+
 
     }
 
